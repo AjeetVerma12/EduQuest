96dfdbd2aa532528c4cd0b767266a41ff854ed3f
AjeetVerma2912 <ajeet120300@gmail.com>
2024-01-19 01:32:38 +0530

    courses page

--- main.js
+++ main.js
@@ -37,3 +37,124 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+// Array of course objects
+const courses = [
+    {
+        image: './images/course1.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course2.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course3.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course4.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course5.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course6.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course7.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course8.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course9.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course10.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course11.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course12.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course13.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course14.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course15.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course16.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course17.jpg',
+        title: 'Responsive Social Media Website UI Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+    {
+        image: './images/course18.jpg',
+        title: 'Responsive Smart Home Website Design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit assumenda eaque molestiae saepe, consectetur qui facere.'
+    },
+
+    // Add more courses here...
+];
+
+// Function to create course HTML
+function createCourseHTML(course) {
+    return `
+        <article class="course">
+            <div class="course__image">
+                <img src="${course.image}">  
+            </div>
+            <div class="course__info">
+                <h4>${course.title}</h4>
+                <p>${course.description}</p>
+                <a href="courses.html" class="btn btn-primary">Learn More</a>
+            </div>
+        </article>
+    `;
+}
+
+// Function to render all courses
+function renderCourses() {
+    const coursesContainer = document.querySelector('.courses__container');
+    coursesContainer.innerHTML = courses.map(createCourseHTML).join('');
+}
+
+// Call renderCourses on page load or when appropriate
+document.addEventListener('DOMContentLoaded', renderCourses);
